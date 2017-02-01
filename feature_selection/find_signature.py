87f8e7d9a91be1f2d0811437df904a192955b214
Andrea Lianto <andreamarsha@gmail.com>
2017-02-01 22:40:01 +0800

    Add decision tree classifier

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -38,6 +38,14 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print acc
 
 
 
